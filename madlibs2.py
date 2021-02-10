b1120a4120752011ed3fab1a5b2572d064245a1a
heba-ali2030 <heba.galaa@gmail.com>
2021-02-10 21:28:39 +0200

    add

--- madlibs2.py
+++ madlibs2.py
@@ -10,4 +10,4 @@ verb = input('choose verb :')
 bird = input('choose a bird :')
 adj = input('choose an adj :')
 
-print(f'one day iI dreamed that I {verb_in_past} my best friend {name}. He shouted {adv} please don\'t {verb} me as You {verb_in_past} and my {animal} ya sater 3lek. my sister had a {bird} in {time} and it is {age} years old now.  she usually consider in her friends as {adj} because they {verb} {adv} and annoyed her.')
+print(f'one day iI dreamed that I {verb_in_past} my best friend {name}. He shouted {adv} please don\'t {verb} me as You {verb_in_past} and my {animal} ya sater 3lek.my sister had a {bird} in {time} and it is {age} years old now. she usually consider in her friends as {adj} because they {verb} {adv} and annoyed her.')
