37059e359d624f628d2a6f1cd9a2b50c87f40196
heba-ali2030 <heba.galaa@gmail.com>
2021-04-21 01:11:39 +0200

    add

--- madlibs.py
+++ madlibs.py
@@ -5,6 +5,6 @@ adj = input('adj')
 animal = input('choose an animal')
 verb1 = input('choose verb1')
 verb2 = input('choose verb2')
-verb3= input('choose verb3')
+verb3 = input('choose verb3')
  
 print(f'It was {food} day at school , and {name} was super {adj} for lunch. but when he went outside to eat, a {animal} stole his {food}! {name} chased the {animal} all over school. he {verb1}, {verb2}, {verb3} through the playground. then he tripped on his {animal} and the {animal} escaped! Luckily {name}s  friends were willing to share their {food} with him.')
---
+++ madlibs3.py
@@ -0,0 +1,16 @@
+#build a mad libs generator, use a while loop to keep your program running for 5 times maximum.
+
+
+def madlib3():
+    game= int(input('Enter number of times you want to play : ')
+    while game < 5:
+        animals= input('enter a animal name : ')
+        profession = input('enter a profession name: ')
+        cloth = input('enter a piece of cloth name: ')
+        things = input('enter a thing name: ')
+        name= input('enter a name: ')
+        place = input('enter a place name: ')
+        verb = input('enter a verb in ing form: ')
+        food = input('food name: ')
+    print('say ' + food + ', the photographer said as the camera flashed! ' + name + ' and I had gone to ' + place +' to get our photos taken on my birthday. The first photo we really wanted was a picture of us dressed as ' + animals + ' pretending to be a ' + profession + '. when we saw the second photo, it was exactly what I wanted. We both looked like ' + things + ' wearing ' + cloth + ' and ' + verb + ' --exactly what I had in mind')
+madlib3()
